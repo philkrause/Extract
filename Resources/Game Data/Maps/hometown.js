c499f785d255ec84f6030fea8fbc4974773e814f
sirdjango <38673429+sirdjango@users.noreply.github.com>
2018-12-12 18:31:18 -0600

    Create hometown.js

---
+++ Resources/Game Data/Maps/hometown.js
@@ -0,0 +1,8 @@
+exports.name = "Home Town";
+exports.room = "rm_map_home";
+
+exports.start_x = 320;
+exports.start_y = 320;
+
+//removes updating each client in different areas   
+exports.clients = []
