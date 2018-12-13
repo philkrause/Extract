21d4f77b6b7d3bd1fd6ccebb3eeda2bcd4de96db
sirdjango <38673429+sirdjango@users.noreply.github.com>
2018-12-12 18:32:50 -0600

    Create 00_packetmodels.js

---
+++ Initializers/00_packetmodels.js
@@ -0,0 +1,26 @@
+var Parser = require('binary-parser').Parser;
+var StringOptions = {length: 99, zeroTerminated : true};
+
+module.exports = PacketModels = {
+
+   header: new Parser().skip(1)
+        .string("command", StringOptions),
+
+    login: new Parser().skip(1)
+        .string("command", StringOptions)
+        .string("username", StringOptions)
+        .string("password", StringOptions),
+
+    register: new Parser().skip(1)
+        .string("command", StringOptions)
+        .string("username", StringOptions)
+        .string("password", StringOptions),
+
+
+    pos: new Parser().skip(1)
+        .string("command", StringOptions)
+        .int32le("target_x")
+        .int32le("target_y")
+        
+    
+}
